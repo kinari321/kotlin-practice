a41048e93e782c1326a22e35736af1432bb0c602
kinari321 <abcdream321@yahoo.co.jp>
2022-10-23 15:04:28 +0900

    p45まで

--- kotlin-server-side-programming-practice/part1/chapter02/src/main/kotlin/Main.kt
+++ kotlin-server-side-programming-practice/part1/chapter02/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() {
 //    printOddEvenNumberText(100)
 //    println(getOddEvenNumberText(99))
 //    val user1 = User1()
@@ -6,7 +6,9 @@ fun main(args: Array<String>) {
 //    val user5 = User5()
 //    user5.name = ""
 //    println(user5.name)
-    val
+    val user7 = User7(1, "Tanaka")
+    user7.name = "Yamada"
+    println(user7)
 }
 
 //fun printOddEvenNumberText(num: Int) {
@@ -41,27 +43,29 @@ fun main(args: Array<String>) {
 //        }
 //}
 //
-class User6 {
-    val id: Int = 1
-    val name = "Kotlin"
+//class User6 {
+//    val id: Int = 1
+//    val name = "Kotlin"
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return true
+//
+//        other as User6
+//
+//        if (id != other.id) return false
+//        if (name != other.name) return false
+//        return true
+//    }
+//
+//    override fun hashCode(): Int {
+//        return 31 * name.hashCode() + id
+//    }
+//
+//    //
+//    override fun toString(): String {
+//        return "User6(id=$id, name=$name)"
+//    }
+//}
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return true
-
-        other as User6
-
-        if (id != other.id) return false
-        if (name != other.name)
-            return
-    }
-
-    override fun hashCode(): Int {
-        return 31 * name.hashCode() + id
-    }
-
-    //
-    override fun toString(): String {
-        return "User6(id=$id, name=$name)"
-    }
-}
+data class User7(val id: Int, var name: String)

591b0b6e388f41b2298630f97be546edacacf8f5
kinari321 <abcdream321@yahoo.co.jp>
2022-11-07 17:48:20 +0900

    p69デリゲートまで

--- kotlin-server-side-programming-practice/part1/chapter02/src/main/kotlin/Main.kt
+++ kotlin-server-side-programming-practice/part1/chapter02/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import java.beans.BeanProperty
+import java.lang.IllegalStateException
+import kotlin.reflect.KProperty
+
 fun main() {
     // スコープ関数
     val list = mutableListOf<Int>()
@@ -6,7 +10,6 @@ fun main() {
     }
     val oddNumbers = list.joinToString(separator = " ")
     println(oddNumbers)
-
 
     // with
     val oddNumbersWith = with(mutableListOf<Int>()) {
@@ -17,7 +20,6 @@ fun main() {
     }
     println(oddNumbersWith)
 
-
     // run
     val oddNumbersRun = mutableListOf<Int>().run {
         for (i in 1..10) {
@@ -27,14 +29,12 @@ fun main() {
     }
     println(oddNumbersRun)
 
-
     // let
     data class User(val name: String)
 
     fun createUserLet(name: String?): User? {
         return if (name != null) User(name) else null
     }
-
 
     // apply(alsoを推奨)
     data class User1(val id: Int, var name: String, var address: String)
@@ -52,7 +52,6 @@ fun main() {
     }
     updateUserApply(100, "TanakaKotlin", "NY")
 
-
     // also
     fun updateUserAlso(id: Int, newName: String, newAddress: String) {
         val userAlso = getUser(id).also { u ->
@@ -61,4 +60,58 @@ fun main() {
         }
         println(userAlso)
     }
+
+    // デリゲート
+    val executorDelegate = AddCalclationExecutorExecutorDelegate(CommonCalculationExecutor())
+    executorDelegate.printStartMessage()
+    println(executorDelegate.calc(8, 11))
+
+    //委譲プロパティ
+    val delegatePerson = DelegatePerson()
+    delegatePerson.name = "tanaka"
+    delegatePerson.address = "Tokyo"
+    println(delegatePerson.address)
+
+}
+
+interface CalclationExecutor {
+    val message: String
+    fun calc(num1: Int, num2: Int): Int
+    fun printStartMessage()
+}
+
+class CommonCalculationExecutor(override val message: String = "calc") : CalclationExecutor {
+    override fun calc(num1: Int, num2: Int): Int {
+        throw IllegalStateException("Not Implements calc")
+    }
+
+    override fun printStartMessage() {
+        println("start $message")
+    }
+}
+
+class AddCalclationExecutorExecutorDelegate(private val calclationExecutor: CalclationExecutor) :
+    CalclationExecutor by calclationExecutor {
+    override fun calc(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
+
+class DelegateWithMessage<T> {
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        println("${property.name}を取得します")
+        return value!!
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        println("${property.name}を更新します")
+        this.value = value
+    }
+}
+
+class DelegatePerson {
+    var name: String by DelegateWithMessage()
+    var address: String by DelegateWithMessage()
 }

aaf40440ded605ebe154eb08d1e88fd8bd9c78bf
kinari321 <abcdream321@yahoo.co.jp>
2022-12-29 12:30:50 +0900

    Initializr -> spring-data-jdbc

---
+++ kotlin-spring-data-jdbc-web-api-demo/src/main/kotlin/com/example/kotlinspringdatajdbcwebapidemo/KotlinSpringDataJdbcWebApiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringdatajdbcwebapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringDataJdbcWebApiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringDataJdbcWebApiDemoApplication>(*args)
+}
---
+++ kotlin-spring-data-jdbc-web-api-demo/src/test/kotlin/com/example/kotlinspringdatajdbcwebapidemo/KotlinSpringDataJdbcWebApiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringdatajdbcwebapidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringDataJdbcWebApiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

04cf8083eee20961f0a094da4ba663d3f265afa5
kinari321 <abcdream321@yahoo.co.jp>
2023-01-28 15:59:48 +0900

    initial thymeleaf helloworld project

---
+++ thymeleaf-helloworld/src/main/kotlin/com/example/thymeleafhelloworld/ThymeleafHelloworldApplication.kt
@@ -0,0 +1,11 @@
+package com.example.thymeleafhelloworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafHelloworldApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThymeleafHelloworldApplication>(*args)
+}
---
+++ thymeleaf-helloworld/src/test/kotlin/com/example/thymeleafhelloworld/ThymeleafHelloworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.thymeleafhelloworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThymeleafHelloworldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
